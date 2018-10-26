a9cfc70ac56b137f8c84f46a154aa03fe541a3a7
manvidas <manvidas@NM-16010-PC>
2018-10-26 14:47:55 +0100

    Added GraphController

---
+++ StoreGraphRenderer/StoreGraphRenderer/Controllers/GraphController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreGraphRenderer.Controllers
+{
+    public class GraphController : Controller
+    {
+        // GET: Graph
+        public ActionResult RenderGraph()
+        {
+
+            return View();
+        }
+    }
+}
