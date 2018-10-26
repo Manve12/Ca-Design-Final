08384c9f5b1277fea25a9bf6e72aab3226bb6cc4
manvidas <manvidas@NM-16010-PC>
2018-10-26 11:06:55 +0100

    Updated DbConnect, added parameters to connect to database and close connection

--- StoreGraphRenderer/StoreGraphRenderer/Data/DbConnect.cs
+++ StoreGraphRenderer/StoreGraphRenderer/Data/DbConnect.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,19 @@ namespace StoreGraphRenderer.Data
 {
     public static class DbConnect
     {
+        public static string Connection { get; } = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public static SqlConnection ConnectionDatabase { get; set; } = new SqlConnection(Connection);
 
+        public static int ConnectionTimeout = 60;
+
+        public static void OpenConnection()
+        {
+            ConnectionDatabase.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            ConnectionDatabase.Close();
+        }
     }
 }
