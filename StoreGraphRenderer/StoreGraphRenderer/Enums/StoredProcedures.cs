d8e0dd33b4bd1c03dc4ec1786798498e4e5bff54
manvidas <manvidas@NM-16010-PC>
2018-10-26 10:52:54 +0100

    Added StoredProcedures class

---
+++ StoreGraphRenderer/StoreGraphRenderer/Enums/StoredProcedures.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreGraphRenderer.Enums
+{
+    public class StoredProcedures
+    {
+    }
+}
