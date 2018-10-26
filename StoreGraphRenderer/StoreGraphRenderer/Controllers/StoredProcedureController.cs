9a866e0441d9fa143b973d2c7c2430b64244c92b
manvidas <manvidas@NM-16010-PC>
2018-10-26 10:52:29 +0100

    Added StoredProcedureController

---
+++ StoreGraphRenderer/StoreGraphRenderer/Controllers/StoredProcedureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreGraphRenderer.Controllers
+{
+    public class StoredProcedureController : Controller
+    {
+        // GET: StoredProcedure
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
